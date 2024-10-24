e1df61cf507ba3db8c84881c8b6cc5113f3c5dfa
Helmi Naufal Ahmad Haryanto <helminaufal2001@gmail.com>
2024-10-24 21:40:39 +0700

    ninth commit

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
     const userInfo = document.querySelector(".user-info");
+
     const userName = prompt("Please enter your name:");
     document.getElementById("user-name").textContent = userName;
 
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Mencegah form dari pengiriman default
+        event.preventDefault();
 
         const name = document.getElementById("name").value;
         const email = document.getElementById("email").value;
         const phone = document.getElementById("phone").value;
         const message = document.getElementById("message").value;
+
+        if (!name || !email || !phone || !message) {
+            alert("Semua bidang harus diisi!");
+            return;
+        }
 
         const userContent = `
             <p>Nama: ${name}</p>
@@ -18,7 +24,9 @@ document.addEventListener("DOMContentLoaded", function() {
             <p>Nomor Telepon: ${phone}</p>
             <p>Pesan: ${message}</p>
         `;
-
         userInfo.innerHTML = userContent;
+
+        form.reset();
     });
 });
+
